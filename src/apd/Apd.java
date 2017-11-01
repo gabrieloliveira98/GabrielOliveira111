40e1fd838d8c96c86e128347799322fdf2bb427d
31614116 <31614116@087891-P33S104.macklabs.br>
2017-11-01 09:56:20 -0200

    Variavel X adicionada

--- src/apd/Apd.java
+++ src/apd/Apd.java
@@ -15,7 +15,8 @@ public class Apd {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int x=10;
+        System.out.println("Variavel x= " + x);
     }
     
 }
